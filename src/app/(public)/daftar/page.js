1d489151bd5021bb26b5e81475bcd8178760a83b
AmaCrisn <galapersada9@gmail.com>
2026-03-31 21:19:23 +0700

    Create registration frontend layout

---
+++ src/app/(public)/daftar/page.js
@@ -0,0 +1,71 @@
+'use client'
+
+import Button from "@/components/ui/button";
+import Stepper from "@/components/layout/stepper"
+import Account from "@/components/layout/steps/account";
+import Academic from "@/components/layout/steps/academic";
+import Biodata from "@/components/layout/steps/biodata";
+import Files from "@/components/layout/steps/files";
+import Finish from "@/components/layout/steps/finish";
+import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import Input from "@/components/ui/input";
+import Checkbox from "@/components/ui/checkbox";
+
+export default function Home() {
+  const [step, setStep] = useState(1);
+  const [biodataStep, setBiodataStep] = useState("student");
+  const contents = [
+    <Account />,
+    <Biodata biodataStep={biodataStep} setBiodataStep={setBiodataStep}/>,
+    <Academic />,
+    <Files />,
+    <Finish />,
+  ];
+
+  const nextStep = () => {
+    // If step 2 then change sub step
+    if (step === 2) {
+      if (biodataStep === "student") {
+        setBiodataStep("parents");
+        return;
+      }
+      
+      if (biodataStep === "parents") {
+        setStep(3);
+        return;
+      }
+    }
+
+    setStep((prev) => Math.min(prev + 1, 5));
+  };
+
+  const prevStep = () => {
+    // If step 2 then change sub step
+    if (step === 2 && biodataStep === "parents") {
+      setBiodataStep("student");
+      return;
+    }
+
+    setStep((prev) => Math.max(prev - 1, 1)); // min step = 1
+  };
+
+  return (
+    <div className="flex justify-center items-center text-(-clr-txt-primary) my-10">
+      <div className="w-full max-w-5xl py-10 bg-(--clr-bg-card) border-2 border-(--clr-border) rounded-3xl shadow-lg flex flex-col gap-5">
+        {/* Progress Stepper */}
+        <div className="px-12">
+          <Stepper currentStep={step} />
+        </div>
+
+        {contents[step - 1]}
+
+        <div className="flex self-end gap-6 mt-6 px-12">
+          <Button variant="secondary" className="px-10" onClick={prevStep} hidden={step === 1}>Sebelumnya</Button>
+          <Button variant="primary" className="px-10" onClick={nextStep}>{step === 4 ? "Selesai" : "Selanjutnya"}</Button>
+        </div>
+      </div>
+    </div>
+  );
+}
