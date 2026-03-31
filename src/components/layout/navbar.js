649974e188bbf3d2f9ff7f86326f15e586ddce21
AmaCrisn <galapersada9@gmail.com>
2026-03-31 12:22:08 +0700

    Redirect pages with buttons and links

--- src/components/layout/navbar.js
+++ src/components/layout/navbar.js
@@ -1,48 +1,52 @@
 'use client'
 
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import Button from "../../components/ui/button"
 import Image from "next/image";
-import { usePathname } from "next/navigation";
 
 export default function navbar() {
-  const pathname = usePathname();
-  const isAuthPage = pathname === "/login" || pathname === "/register";
+  const router = useRouter();
   return (
     <div>
       {/* Header */}
       <nav className="bg-[var(--clr-primary)] text-white px-32 pt-6">
         {/* Top Section - title and buttons*/}
-        <div className="flex items-center justify-between mx-6 mb-3.5 text-2xl font-bold">
+        <div className="flex items-center justify-between mx-6 mb-3.5 font-semibold">
           {/* Title */}
           <div className="flex items-center gap-4">
             <div className="bg-white rounded-3xl p-2">
               <Image loading="eager" alt="Logo Trilogi" height={80} width={80} src="/Universitas_Trilogi_logo.svg"></Image>
             </div>
-            Universitas Trilogi
+            
+            <div>
+              <p className="text-sm font-light">Penerimaan Mahasiswa Baru</p>
+              <h1 className="text-3xl">Universitas Trilogi</h1>
+            </div>
           </div>
 
           {/* Button regist and login section */}
-          <div className="flex gap-6">
-            <button className="px-10 py-2.5 rounded-md hover:bg-[var(--clr-ghost-hover)] hover:cursor-pointer transition-all duration-300 ease-in-out">
+          <div className="flex gap-6 text-2xl">
+            <Button onClick={() => {router.push("/masuk")}} variant="ghost" className="px-10">
               Masuk
-            </button>
-            <button className="bg-[var(--clr-accent)] px-10 py-2.5 rounded-md hover:bg-[var(--clr-accent-hover)] hover:cursor-pointer transition-all duration-300 ease-in-out">
+            </Button>
+            <Button onClick={() => {router.push("/daftar")}} variant="accent" className="px-10">
               Daftar
-            </button>
+            </Button>
           </div>
         </div>
 
         {/* Bottom Section - navigation purposes */}
         <div className="flex gap-6 transition-all text-lg">
-          <div className="px-4 py-2 rounded-t-md hover:bg-[var(--clr-primary-hover)] hover:cursor-pointer duration-300 ease-in-out">
+          <Link href="/" className="px-4 py-2 rounded-t-md hover:bg-[var(--clr-primary-hover)] hover:cursor-pointer duration-300 ease-in-out">
             Beranda
-          </div>
-          <div className="px-4 py-2 rounded-t-md hover:bg-[var(--clr-primary-hover)] hover:cursor-pointer duration-300 ease-in-out">
+          </Link>
+          <Link href="/informasi" className="px-4 py-2 rounded-t-md hover:bg-[var(--clr-primary-hover)] hover:cursor-pointer duration-300 ease-in-out">
             Informasi Pendaftaran
-          </div>
-          <div className="px-4 py-2 rounded-t-md hover:bg-[var(--clr-primary-hover)] hover:cursor-pointer duration-300 ease-in-out">
+          </Link>
+          <Link href="/dosen" className="px-4 py-2 rounded-t-md hover:bg-[var(--clr-primary-hover)] hover:cursor-pointer duration-300 ease-in-out">
             Dosen
-          </div>
+          </Link>
         </div>
       </nav>
     </div>
