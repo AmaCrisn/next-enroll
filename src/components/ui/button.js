6d6abb54aa7a715e5199470ebc4dabcdb59a80eb
AmaCrisn <galapersada9@gmail.com>
2026-03-31 16:26:49 +0700

    Cleaned styling class

--- src/components/ui/button.js
+++ src/components/ui/button.js
@@ -3,10 +3,10 @@ import React from "react";
 export default function Button({ variant = "primary", className = "", children, ...props }) {
   const base = "py-2.5 rounded-md font-bold transition-all duration-300 active:duration-100 hover:cursor-pointer";
   const variants = {
-    primary: "bg-[var(--clr-primary)] hover:bg-[var(--clr-primary-hover)] text-white active active:bg-[var(--clr-primary-active)]",
-    secondary: "border-[var(--clr-primary)] hover:border-[var(--clr-primary-hover)] hover:bg-[var(--clr-primary-hover)] active:bg-[var(--clr-primary-active)] active:border-[var(--clr-primary-active)] hover:text-white border-2",
-    ghost: "bg-[var(--clr-ghost)] hover:bg-[var(--clr-ghost-hover)] active:bg-[var(--clr-ghost-active)]",
-    accent: "bg-[var(--clr-accent)] hover:bg-[var(--clr-accent-hover)] active:bg-[var(--clr-accent-active)]",
+    primary: "bg-(--clr-primary) hover:bg-(--clr-primary-hover) text-white active active:bg-(--clr-primary-active)",
+    secondary: "border-(--clr-primary) hover:border-(--clr-primary-hover) hover:bg-(--clr-primary-hover) active:bg-(--clr-primary-active) active:border-(--clr-primary-active) hover:text-white border-2",
+    ghost: "bg-(--clr-ghost) hover:bg-(--clr-ghost-hover) active:bg-(--clr-ghost-active)",
+    accent: "bg-(--clr-accent) hover:bg-(--clr-accent-hover) active:bg-(--clr-accent-active)",
   };
 
   return (
