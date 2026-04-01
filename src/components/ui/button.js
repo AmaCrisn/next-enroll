4ab44971e92837d69eefb5ee0b62d6b4fdf4125f
AmaCrisn <galapersada9@gmail.com>
2026-04-01 22:25:46 +0700

    Fixed focus styling

--- src/components/ui/button.js
+++ src/components/ui/button.js
@@ -1,12 +1,32 @@
 import React from "react";
 
 export default function Button({ variant = "primary", className = "", children, ...props }) {
-  const base = "py-2.5 my-2 rounded-md font-bold transition-all duration-300 active:duration-100 hover:cursor-pointer focus:outline-none";
+  const base = "py-2.5 my-2 rounded-md font-bold transition-all duration-300 active:duration-100 border-2 hover:cursor-pointer focus:outline-none";
   const variants = {
-    primary: "bg-(--clr-primary) hover:bg-(--clr-primary-hover) text-white active active:bg-(--clr-primary-active) border-(--clr-primary-hover) focus:border-2",
-    secondary: "border-(--clr-primary) hover:border-(--clr-primary-hover) hover:bg-(--clr-primary-hover) hover:text-white active:bg-(--clr-primary-active) active:border-(--clr-primary-active) focus:border-(--clr-primary-hover) border-2",
-    ghost: "bg-(--clr-ghost) hover:bg-(--clr-ghost-hover) active:bg-(--clr-ghost-active) focus:border-2 border-(--clr-ghost-hover)",
-    accent: "bg-(--clr-accent) hover:bg-(--clr-accent-hover) active:bg-(--clr-accent-active) border-(--clr-accent-hover) focus:border-2",
+    primary:
+      "bg-(--clr-primary) text-white border-(--clr-primary) " +
+      "hover:bg-(--clr-primary-hover) " +
+      "hover:border-(--clr-primary-hover) " +
+      "active:bg-(--clr-primary-active) " +
+      "focus-visible:outline-none focus-visible:border-(--clr-primary-hover)",
+
+    secondary:
+      " border-(--clr-primary)" +
+      "hover:border-(--clr-primary-hover) hover:bg-[var(--clr-primary-hover) hover:text-white " +
+      "active:bg-(--clr-primary-active) active:border-[var(--clr-primary-active) " +
+      "focus-visible:outline-none focus-visible:border-(--clr-primary-hover)",
+
+    ghost:
+      "bg-(--clr-ghost) border-transparent " +
+      "hover:bg-(--clr-ghost-hover) " +
+      "active:bg-(--clr-ghost-active) " +
+      "focus-visible:outline-none focus-visible:border-(--clr-ghost-hover)",
+
+    accent:
+      "bg-(--clr-accent) border-transparent " +
+      "hover:bg-(--clr-accent-hover) " +
+      "active:bg-(--clr-accent-active) " +
+      "focus-visible:outline-none focus-visible:border-(--clr-accent-hover)",
   };
 
   return (
