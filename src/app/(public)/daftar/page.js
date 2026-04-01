0e43b8fcc7e28d03d66272b68caafbd987527ee2
AmaCrisn <galapersada9@gmail.com>
2026-04-01 21:11:45 +0700

    Added navigations UX and confirmation step

--- src/app/(public)/daftar/page.js
+++ src/app/(public)/daftar/page.js
@@ -8,40 +8,48 @@ import Biodata from "@/components/layout/steps/biodata";
 import Files from "@/components/layout/steps/files";
 import Finish from "@/components/layout/steps/finish";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
 import Input from "@/components/ui/input";
 import Checkbox from "@/components/ui/checkbox";
+import Confirmation from "@/components/layout/steps/confirmation";
 
 export default function Home() {
+  const router = useRouter();
   const [step, setStep] = useState(1);
   const [biodataStep, setBiodataStep] = useState("student");
   const contents = [
     <Account />,
-    <Biodata biodataStep={biodataStep} setBiodataStep={setBiodataStep}/>,
+    <Biodata biodataStep={biodataStep} setBiodataStep={setBiodataStep} />,
     <Academic />,
     <Files />,
+    <Confirmation />,
     <Finish />,
   ];
 
   const nextStep = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" })
+    
     // If step 2 then change sub step
     if (step === 2) {
       if (biodataStep === "student") {
         setBiodataStep("parents");
         return;
       }
-      
+
       if (biodataStep === "parents") {
         setStep(3);
         return;
       }
     }
-
-    setStep((prev) => Math.min(prev + 1, 5));
+    
+    setStep((prev) => Math.min(prev + 1, 6));
   };
 
   const prevStep = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" })
+
     // If step 2 then change sub step
     if (step === 2 && biodataStep === "parents") {
       setBiodataStep("student");
@@ -62,8 +70,13 @@ export default function Home() {
         {contents[step - 1]}
 
         <div className="flex self-end gap-6 mt-6 px-12">
-          <Button variant="secondary" className="px-10" onClick={prevStep} hidden={step === 1}>Sebelumnya</Button>
-          <Button variant="primary" className="px-10" onClick={nextStep}>{step === 4 ? "Selesai" : "Selanjutnya"}</Button>
+          <Button variant="secondary" className="px-10" onClick={prevStep} hidden={step === 1}>
+            Sebelumnya
+          </Button>
+          <Button variant="primary" className="px-10"
+            onClick={step === 6 ? () => router.push("/masuk") : nextStep}>
+            {step === 6 ? "Masuk" : step === 5 ? "Selesai" : "Selanjutnya"}
+          </Button>
         </div>
       </div>
     </div>
