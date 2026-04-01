e84e97cc840f24d2114db6eea7d8e5c600845e48
AmaCrisn <galapersada9@gmail.com>
2026-04-01 20:58:20 +0700

    Add focus styling

--- src/components/ui/button.js
+++ src/components/ui/button.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 export default function Button({ variant = "primary", className = "", children, ...props }) {
-  const base = "py-2.5 rounded-md font-bold transition-all duration-300 active:duration-100 hover:cursor-pointer";
+  const base = "py-2.5 my-2 rounded-md font-bold transition-all duration-300 active:duration-100 hover:cursor-pointer focus:outline-none";
   const variants = {
-    primary: "bg-(--clr-primary) hover:bg-(--clr-primary-hover) text-white active active:bg-(--clr-primary-active)",
-    secondary: "border-(--clr-primary) hover:border-(--clr-primary-hover) hover:bg-(--clr-primary-hover) active:bg-(--clr-primary-active) active:border-(--clr-primary-active) hover:text-white border-2",
-    ghost: "bg-(--clr-ghost) hover:bg-(--clr-ghost-hover) active:bg-(--clr-ghost-active)",
-    accent: "bg-(--clr-accent) hover:bg-(--clr-accent-hover) active:bg-(--clr-accent-active)",
+    primary: "bg-(--clr-primary) hover:bg-(--clr-primary-hover) text-white active active:bg-(--clr-primary-active) border-(--clr-primary-hover) focus:border-2",
+    secondary: "border-(--clr-primary) hover:border-(--clr-primary-hover) hover:bg-(--clr-primary-hover) hover:text-white active:bg-(--clr-primary-active) active:border-(--clr-primary-active) focus:border-(--clr-primary-hover) border-2",
+    ghost: "bg-(--clr-ghost) hover:bg-(--clr-ghost-hover) active:bg-(--clr-ghost-active) focus:border-2 border-(--clr-ghost-hover)",
+    accent: "bg-(--clr-accent) hover:bg-(--clr-accent-hover) active:bg-(--clr-accent-active) border-(--clr-accent-hover) focus:border-2",
   };
 
   return (
